99461e6dd63ab0da6d8ec56d1bd68be76306e428
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-02-28 13:55:08 +0300

    small fix

--- app/src/main/java/com/dashkevich/weather/MainActivity.kt
+++ app/src/main/java/com/dashkevich/weather/MainActivity.kt
@@ -12,10 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
-        binding.textView.text = "°"
     }
 }
